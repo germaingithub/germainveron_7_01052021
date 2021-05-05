84dadcee63eb23fa950867f0bb241b64d26d169d
Germaingit <germain.veron@me.com>
2021-05-05 15:38:49 +0200

    backend ok

--- groupomania-project-app/apiRouter.js
+++ groupomania-project-app/apiRouter.js
@@ -1,7 +1,8 @@
 // import
 const express = require('express');
 const usersCtrl = require('./routes/usersCtrl');
-
+const messagesCtrl = require('./routes/messagesCtrl');
+const likesCtrl = require('./routes/likesCtrl');
 //Router
 exports.router = (function() {
   const apiRouter = express.Router();
@@ -11,6 +12,14 @@ exports.router = (function() {
     apiRouter.route('/users/login/').post(usersCtrl.login);
     apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
     apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+    //Messages routes
+    apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+    apiRouter.route('/messages/').get(messagesCtrl.listMessages);
+
+    //likes
+    apiRouter.route('/messages/:messageId/vote/like').post(likesCtrl.likePost);
+    apiRouter.route('/messages/:messageId/vote/dislike').post(likesCtrl.dislikePost);
 
     return apiRouter;
 })();
--- groupomania-project-app/routes/messagesCtrl.js
+++ groupomania-project-app/routes/messagesCtrl.js
@@ -5,27 +5,95 @@ const jwtUtils = require('../utils/jwt.utils')
 //constants
 const TITLE_LIMIT = 2;
 const CONTENT_LIMIT = 4;
-
+const ITEMS_LIMIT = 50;
 //routes
-module.exports ={
-    createMessage: function (req, res) {
-        //getting auth header
-        var headerAuth = req.headers["authorization"];
-        var userId = jwtUtils.getUserId(headerAuth);
+module.exports = {
+  createMessage: function(req, res) {
+    //getting auth header
+    var headerAuth = req.headers["authorization"];
+    var userId = jwtUtils.getUserId(headerAuth);
 
-        //params
-        const title = req.body.title;
-        const content = req.body.content;
+    //params
+    const title = req.body.title;
+    const content = req.body.content;
 
-        if (title == null || content == null) {
-            return res.status(400).json({ 'error': 'bad request'});
-        }
-        
-        if (title.length <= TITLE_LIMIT || content.length <= CONTENT_LIMIT) {
-          return res.status(400).json({ error: "invalid parameters" });
-        }
-    },
-    listMessages: function(req, res) {
-        
+    if (title == null || content == null) {
+      return res.status(400).json({ error: "bad request" });
     }
-}
+
+    if (title.length <= TITLE_LIMIT || content.length <= CONTENT_LIMIT) {
+      return res.status(400).json({ error: "invalid parameters" });
+    }
+
+    asyncLib.waterfall(
+      [
+        function(done) {
+          models.User.findOne({
+            where: { id: userId },
+          })
+            .then(function(userFound) {
+              done(null, userFound);
+            })
+            .catch(function(err) {
+              return res.status(500).json({ error: "unable to verify user" });
+            });
+        },
+        function(userFound, done) {
+          if (userFound) {
+            models.Message.create({
+              title: title,
+              content: content,
+              likes: 0,
+              UserId: userFound.id,
+            }).then(function(newMessage) {
+              done(newMessage);
+            });
+          } else {
+            res.status(404).json({ error: "user not found" });
+          }
+        },
+      ],
+      function(newMessage) {
+        if (newMessage) {
+          return res.status(201).json(newMessage);
+        } else {
+          return res.status(500).json({ error: "cannot post message" });
+        }
+      }
+    );
+  },
+  listMessages: function(req, res) {
+    var fields = req.query.fields;
+    var limit = parseInt(req.query.limit);
+    var offset = parseInt(req.query.offset);
+    var order = req.query.order;
+
+    if (limit > ITEMS_LIMIT) {
+      limit = ITEMS_LIMIT;
+    }
+
+    models.Message.findAll({
+      order: [order != null ? order.split(":") : ["title", "ASC"]],
+      attributes: fields !== "*" && fields != null ? fields.split(",") : null,
+      limit: !isNaN(limit) ? limit : null,
+      offset: !isNaN(offset) ? offset : null,
+      include: [
+        {
+          model: models.User,
+          attributes: ["username"],
+        },
+      ],
+    })
+      .then(function(messages) {
+        if (messages) {
+          res.status(200).json(messages);
+        } else {
+          res.status(404).json({ error: "no mesccsages found" });
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.status(500).json({ error: "invalid fields" });
+      });
+  },
+};
